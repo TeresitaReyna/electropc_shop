f14f2e9704cb07ea51ae5720dadbe49c8c5a3d64
SagittareAlex <sagittareivan@hotmail.com>
2022-05-23 17:05:46 -0500

    manage products traducido

--- admin/manage-products.php
+++ admin/manage-products.php
@@ -22,7 +22,7 @@ if(isset($_GET['del']))
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Admin| Manage Products</title>
+	<title>Admin | Gestionar Productos</title>
 	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link type="text/css" href="css/theme.css" rel="stylesheet">
@@ -41,14 +41,14 @@ if(isset($_GET['del']))
 
 	<div class="module">
 							<div class="module-head">
-								<h3>Manage Products</h3>
+								<h3>Gestionar Productos</h3>
 							</div>
 							<div class="module-body table">
 	<?php if(isset($_GET['del']))
 {?>
 									<div class="alert alert-error">
 										<button type="button" class="close" data-dismiss="alert">×</button>
-									<strong>Oh snap!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
+									<strong>Oh rayos!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
 									</div>
 <?php } ?>
 
@@ -59,12 +59,12 @@ if(isset($_GET['del']))
 									<thead>
 										<tr>
 											<th>#</th>
-											<th>Product Name</th>
-											<th>Category </th>
-											<th>Subcategory</th>
-											<th>Company Name</th>
-											<th>Product Creation Date</th>
-											<th>Action</th>
+											<th>Nombre de Producto</th>
+											<th>Categoría</th>
+											<th>Subcategoría</th>
+											<th>Nombre de Compañía</th>
+											<th>Fecha de Creación de Producto</th>
+											<th>Acción</th>
 										</tr>
 									</thead>
 									<tbody>
