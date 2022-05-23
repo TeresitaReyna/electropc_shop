c57f019a92975232c60351f6ff6fd526af90842d
SagittareAlex <sagittareivan@hotmail.com>
2022-05-23 16:01:59 -0500

    track order traducido

--- track-order.php
+++ track-order.php
@@ -17,7 +17,7 @@ window.print();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Order Tracking Details</title>
+<title>Detalles de Seguimiento</title>
 <link href="style.css" rel="stylesheet" type="text/css" />
 <link href="anuj.css" rel="stylesheet" type="text/css">
 </head>
@@ -28,11 +28,11 @@ window.print();
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
 
     <tr height="50">
-      <td colspan="2" class="fontkink2" style="padding-left:0px;"><div class="fontpink2"> <b>Order Tracking Details !</b></div></td>
+      <td colspan="2" class="fontkink2" style="padding-left:0px;"><div class="fontpink2"> <b>Detalles de Seguimiento !</b></div></td>
       
     </tr>
     <tr height="30">
-      <td  class="fontkink1"><b>order Id:</b></td>
+      <td  class="fontkink1"><b>Id de orden:</b></td>
       <td  class="fontkink"><?php echo $oid;?></td>
     </tr>
     <?php 
@@ -47,15 +47,15 @@ while($row=mysqli_fetch_array($ret))
     
     
       <tr height="20">
-      <td class="fontkink1" ><b>At Date:</b></td>
+      <td class="fontkink1" ><b>En Fecha:</b></td>
       <td  class="fontkink"><?php echo $row['postingDate'];?></td>
     </tr>
      <tr height="20">
-      <td  class="fontkink1"><b>Status:</b></td>
+      <td  class="fontkink1"><b>Estado:</b></td>
       <td  class="fontkink"><?php echo $row['status'];?></td>
     </tr>
      <tr height="20">
-      <td  class="fontkink1"><b>Remark:</b></td>
+      <td  class="fontkink1"><b>Observación:</b></td>
       <td  class="fontkink"><?php echo $row['remark'];?></td>
     </tr>
 
@@ -67,7 +67,7 @@ while($row=mysqli_fetch_array($ret))
 else{
    ?>
    <tr>
-   <td colspan="2">Order Not Process Yet</td>
+   <td colspan="2">Orden aún no procesada</td>
    </tr>
    <?php  }
 $st='Delivered';
@@ -79,7 +79,7 @@ $st='Delivered';
      if($st==$currrentSt)
      { ?>
    <tr><td colspan="2"><b>
-      Product Delivered successfully </b></td>
+      Producto entragado con éxito </b></td>
    <?php } 
  
   ?>
