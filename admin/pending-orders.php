2a9179440b63cb0594d7fb617636779724b82574
TeresitaReyna <teresitareyna.epsanol@hotmail.com>
2022-05-24 03:51:14 -0500

    Tablitas

--- admin/pending-orders.php
+++ admin/pending-orders.php
@@ -22,6 +22,7 @@ $currentTime = date( 'd-m-Y h:i:s A', time () );
 	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link type="text/css" href="css/theme.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/customtheme.css">
 	<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
 	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
 	<script language="javascript" type="text/javascript">
@@ -45,7 +46,7 @@ popUpWin = open(URLStr,'popUpWin', 'toolbar=no,location=no,directories=no,status
 			<div class="row row__tablita">
 <?php include('include/sidebar.php');?>			
 			<div class="span9">
-				
+
 					<div class="content tablita__content">
 
 	<div class="module">
